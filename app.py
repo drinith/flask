01854ebc89505d168e23b86c3ef1c05099745f84
Your Name <you@example.com>
2020-02-05 16:51:41 -0300

    Teste

---
+++ app.py
@@ -0,0 +1,21 @@
+import db
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    html =['<ul>']
+
+    for username, user in db.users.items():
+
+        html.append(
+            f"<li><a href='/user/{username}'>{user['name']}</a></li>'"
+        )
+    html.append('</ul>')
+    return '\n'.join(html)
+
+    
+app.run()
