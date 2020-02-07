8d5ff1a4a41949a26033a0cb5288eba943ed1588
Your Name <you@example.com>
2020-02-07 17:21:50 -0300

    mensagem

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import db
-from flask import Flask
+from flask import Flask,abort,url_for
 
 
 app = Flask(__name__)
@@ -12,9 +12,26 @@ def index():
     for username, user in db.users.items():
 
         html.append(
-            f"<li><a href='/user/{username}'>{user['name']}</a></li>"
+            f"<li><a href='{url_for('user',username=username)}'>{user['name']}</a></li>"
         )
     html.append('</ul>')
     return '\n'.join(html)
+
+
+def profile(username):
+    user = db.users.get(username)
+
+    if(user):
+        return f"""
+            <h1>{user['name']}</h1>
+            <img src="{user['image']}"/><br/>
+            telefone:{user['tel']}<br/>
+            <a href="/">Voltar</a>
+        """
+    else:
+        return abort(404,"User not found")
     
-app.run()
+
+app.add_url_rule('/profile/<username>/',view_func=profile,endpoint='user')
+
+app.run(use_reloader=True)
